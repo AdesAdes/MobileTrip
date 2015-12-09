7cde975b5366bd9a70d2e59dbb97c52114c0de90
AdesAdes <mrnrades@gmail.com>
2015-12-09 17:15:48 -0600

    Cambios2.4
    Cambios2.4

--- index.php
+++ index.php
@@ -1,132 +1,123 @@
- <?php 
-           require("Update.php");
- ?>
+<?php 
+
+?>
 <!DOCTYPE html>
 <html>
-	<head>
+<head>
 		<title>Progamacion Movil en Web</title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, user-scalable=no">
 		<link rel="stylesheet" type="text/css" href="style.css">
-		
-	</head>
-	<body style="background: #519839;" id="contain">
-	   
-		 <div>
-		 	<button><a href="graficar_Eventos.php">Graficar</a></button>
-		 </div>
+</head>
+<body style="background: #519839;" id="contain">
 
+		<div id="container">
+		<button>Graficar</button>
+		</div>
+		<div id="div">
 
-		 <div id="div">
-		 </div>
-   </body>
-   <script> 					  
-	var countLS = 0;
-    var countPtr = 0;
-    var test = window.matchMedia("(orientation: portrait)");//Le enviamos como parametro portrait para que este detectandos
-    var colores = ["#89609E","#B04632", "#0079BF", "#D29034", "#519839"];
- 	var indiceColor = 0;
- 	var contain = document.getElementById("div");
-	document.addEventListener('touchstart', manejadorTouchstart, false);
-	document.addEventListener('touchmove', manejadorTouchmove, false);
-	document.addEventListener('touchend', manejadorTouchend, false);
-
-
-	/*function cambiarColor(){
-
-	}*/
-
-	var Handle_Mi_Timer = null;
-    var Contador = 0;	
-	var EventoActual;
-	var noDedos
-
-	function manejadorTouchstart(event){
-        setTimeout(Mi_Timer, 300);
-        EventoActual = "START";
-        noDedos = event.targetTouches.length;
-       // document.getElementsByTagName("body")[0].style ='transition: background-color 4s ease, height 3s ease-in 2s;';
-        document.getElementsByTagName("body")[0].style.background = colores[indiceColor];
-        indiceColor++;
-        if(indiceColor==5){ indiceColor=0};
-       	}
-
-
-	function manejadorTouchmove(event){
-
-		  if(event.targetTouches.length<=5){
-                 contain.innerHTML = "SWIP"+event.targetTouches.length;
-		         EventoActual = "SWIP";
-		  }
-		  if(event.targetTouches.length>5){
-               contain.innerHTML = "MultiSwip"+event.targetTouches.length;
-		       EventoActual = "SWIP";
-
-		  }
-		
-	}
+		</div>
+</body>
+<script> 					  
+		var countLS = 0;
+		var countPtr = 0;
+		var test = window.matchMedia("(orientation: portrait)");//Le enviamos como parametro portrait para que este detectandos
+		var colores = ["#89609E","#B04632", "#0079BF", "#D29034", "#519839"];
+		var indiceColor = 0;
+		var contain = document.getElementById("div");
+		document.addEventListener('touchstart', manejadorTouchstart, false);
+		document.addEventListener('touchmove', manejadorTouchmove, false);
+		document.addEventListener('touchend', manejadorTouchend, false);
 
 
 
-    function manejadorTouchend(event) {
-		if (Contador<1 && EventoActual!="TOUCH" && EventoActual!="SWIP"){
-			window.clearInterval(Handle_Mi_Timer);
-        	Handle_Mi_Timer = null;
-			contain.innerHTML = "TAP";
-			<?php  ModificarTap();?>
-        	Contador = 0;
-        	EventoActual = "TAP";
-        	if(indiceColor==5){
-                        indiceColor=0;
-                     }
+		var Handle_Mi_Timer = null;
+		var Contador = 0;	
+		var EventoActual;
+		var noDedos
+
+		function manejadorTouchstart(event){
+			setTimeout(Mi_Timer, 300);
+			EventoActual = "START";
+			noDedos = event.targetTouches.length;
+			document.getElementsByTagName("body")[0].style.background = colores[indiceColor];
+			indiceColor++;
+			if(indiceColor==5){ indiceColor=0};
 		}
-		Contador = 0;
-		if(event.targetTouches.length>=1){
+
+		function manejadorTouchmove(event){
+
+				if(event.targetTouches.length<=5){
+				contain.innerHTML = "SWIP";
+				contain.innerHTML += event.targetTouches.length;
+				EventoActual = "SWIP";
+				}
+				if(event.targetTouches.length>5){
+				contain.innerHTML = "MultiSwip"+event.targetTouches.length;
+				EventoActual = "SWIP";
+				}
+
+		}
+
+
+		function manejadorTouchend(event) {
+			if (Contador<1 && EventoActual!="TOUCH" && EventoActual!="SWIP"){
+			window.clearInterval(Handle_Mi_Timer);
+			Handle_Mi_Timer = null;
+			contain.innerHTML = "TAP";
+
+			Contador = 0;
+			EventoActual = "TAP";
+			if(indiceColor==5){
+			indiceColor=0;
+			}
+			}
+			Contador = 0;
+			if(event.targetTouches.length>=1){
 			if(EventoActual=="TOUCH"){
-				contain.innerHTML = "TOUCH"+ event.targetTouches.length;	
+			contain.innerHTML = "TOUCH"+ event.targetTouches.length;	
 			}
 			if(indiceColor==5){
-                        indiceColor=0;
-                     }
-		}	
-	}
+			indiceColor=0;
+			}
+			}	
+		}
 
 
-	function Mi_Timer() {
-        Contador++;
-        if(Contador>=1 && EventoActual!="TAP" && EventoActual!="SWIP"){
-        	contain.innerHTML = "TOUCH"+ noDedos;
-        	window.clearInterval(Handle_Mi_Timer);
-        	Handle_Mi_Timer = null;
-        	EventoActual="TOUCH";
-        }
-        Contador = 0;
-    }
-   
+		function Mi_Timer() {
+			Contador++;
+			if(Contador>=1 && EventoActual!="TAP" && EventoActual!="SWIP"){
+			contain.innerHTML = "TOUCH"+ noDedos;
+			window.clearInterval(Handle_Mi_Timer);
+			Handle_Mi_Timer = null;
+			EventoActual="TOUCH";
+			}
+			Contador = 0;
+		}
 
-       orientacion();
-      function orientacion(){
-       	test.addListener(function(evento) {
-				  if(evento.matches)   { //Detecta en que tipo de direccion esta
-				    document.getElementById('div').innerHTML= "PORTRAIT";
-				    countPtr +=1;
-					//alert("port: " + countPtr);     
-                   document.getElementsByTagName("body")[0].style.background = colores[indiceColor];
-        indiceColor++;
-        if(indiceColor==5){ indiceColor=0};
-				  }
-				  else {
-				    document.getElementById('div').innerHTML= "LANDSCAPE";
-					countLS +=1; //Cuando detecte aumenta a 1
-					//alert("land: " + countLS);.		
-		 document.getElementsByTagName("body")[0].style.background = colores[indiceColor];
-         indiceColor++;
-        if(indiceColor==5){ indiceColor=0};			
+
 		
-				  }
-				});
+		function orientacion(){
+				test.addListener(function(evento) {
+				if(evento.matches)   { //Detecta en que tipo de direccion esta
+						document.getElementById('div').innerHTML= "PORTRAIT";
+						countPtr +=1;
 
-  }
+						document.getElementsByTagName("body")[0].style.background = colores[indiceColor];
+						indiceColor++;
+						if(indiceColor==5){ indiceColor=0};
+				}
+				else {
+						document.getElementById('div').innerHTML= "LANDSCAPE";
+						countLS +=1; //Cuando detecte aumenta a 1	
+						document.getElementsByTagName("body")[0].style.background = colores[indiceColor];
+						indiceColor++;
+						if(indiceColor==5){ indiceColor=0};			
 
-		</script>
+				}
+		});
+
+		}
+
+</script>
 </html>
